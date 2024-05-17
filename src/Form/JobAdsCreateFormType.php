ed2bd3832670dd1a21742b28f4a5a4147f7b09e7
batsaikhan <batsaikhan@mongolcontent.mn>
2024-05-17 16:20:46 +0800

    created at fix on jobads

--- src/Form/JobAdsCreateFormType.php
+++ src/Form/JobAdsCreateFormType.php
@@ -19,54 +19,71 @@ class JobAdsCreateFormType extends AbstractType
         $builder
             ->add('title', TextType::class, array(
                 'label' => 'Гарчиг(Монгол)',
+                'required' => false,
+
                 'attr' => array(
                     "class" => "form-control",
                 )
             ))
             ->add('enTitle', TextType::class, array(
                 'label' => 'Гарчиг(Англи)',
+                'required' => false,
+
                 'attr' => array(
                     "class" => "form-control",
                 )
             ))
             ->add('cnTitle', TextType::class, array(
                 'label' => 'Гарчиг(Хятад)',
+                'required' => false,
+
                 'attr' => array(
                     "class" => "form-control",
                 )
             ))
             ->add('profession', TextType::class, array(
                 'label' => 'Ажил(Монгол)',
+                'required' => false,
+
                 'attr' => array(
                     "class" => "form-control",
                 )
             ))
             ->add('enProfession', TextType::class, array(
                 'label' => 'Ажил(Англи)',
+                'required' => false,
+
                 'attr' => array(
                     "class" => "form-control",
                 )
             ))
             ->add('cnProfession', TextType::class, array(
                 'label' => 'Ажил(Хятад)',
+                'required' => false,
+
                 'attr' => array(
                     "class" => "form-control",
                 )
             ))
             ->add('body', CKEditorType::class, array(
                 'label' => 'Агуулга(Монгол)',
+                'required' => false,
+
                 'attr' => array(
                     "class" => "form-control",
                 )
             ))
             ->add('enBody', CKEditorType::class, array(
                 'label' => 'Агуулга(Англи)',
+                'required' => false,
+
                 'attr' => array(
                     "class" => "form-control",
                 )
             ))
             ->add('cnBody', CKEditorType::class, array(
                 'label' => 'Агуулга(Хятад)',
+                'required' => false,
                 'attr' => array(
                     "class" => "form-control",
                 )
